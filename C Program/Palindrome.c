24b1d5e4bbbd8b831f608889026ae44ad850a22d
rajeshwariramya <103486631+rajeshwariramya@users.noreply.github.com>
2024-10-18 19:46:22 +0530

    Update Palindrome.c
    Added \n to the output statements for better readability.
    Confirmed that the function signature is appropriate for its intended use.

--- C Program/Palindrome.c
+++ C Program/Palindrome.c
@@ -1,25 +1,25 @@
-#include <stdio.h> 
-#include <string.h> 
-  
+#include <stdio.h>
+#include <string.h>
+
 // A function to check if a string str is palindrome 
 void isPalindrome(char str[]) 
 { 
     // Start from leftmost and rightmost corners of str 
     int l = 0; 
     int h = strlen(str) - 1; 
-  
+
     // Keep comparing characters while they are same 
     while (h > l) 
     { 
         if (str[l++] != str[h--]) 
         { 
-            printf("%s is Not Palindrome", str); 
+            printf("%s is Not Palindrome\n", str); 
             return; 
         } 
     } 
-    printf("%s is palindrome", str); 
+    printf("%s is Palindrome\n", str); 
 } 
-  
+
 // Driver program to test above function 
 int main() 
 { 
@@ -27,4 +27,4 @@ int main()
     isPalindrome("abbccbba"); 
     isPalindrome("geeks"); 
     return 0; 
-}
+} 
