d6e9ff2cfd62f9b572005662dd34000b2fc5fd10
rajeshwariramya <103486631+rajeshwariramya@users.noreply.github.com>
2024-10-19 09:32:44 +0530

    Update Linear_Search.c

--- C Program/Linear_Search.c
+++ C Program/Linear_Search.c
@@ -3,27 +3,29 @@
 // return -1
  
 #include <stdio.h>
- 
-int search(int arr[], int n, int x)
-{
-    int i;
-    for (i = 0; i < n; i++)
-        if (arr[i] == x)
-            return i;
-    return -1;
+
+int search(int arr[], int n, int x) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == x) {
+            return i; // Element found
+        }
+    }
+    return -1; // Element not found
 }
- 
+
 // Driver code
-int main(void)
-{
+int main(void) {
     int arr[] = { 2, 3, 4, 10, 40 };
     int x = 10;
     int n = sizeof(arr) / sizeof(arr[0]);
-   
+
     // Function call
     int result = search(arr, n, x);
-    (result == -1)
-        ? printf("Not Present")
-        : printf("Present at index %d", result);
-    return 0;
+    if (result == -1) {
+        printf("Not Present\n");
+    } else {
+        printf("Present at index %d\n", result);
+    }
+
+    return 0; // Return success
 }
