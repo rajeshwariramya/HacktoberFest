6e7acd85926dadba8af8cf94ea2358610efd2d5b
rajeshwariramya <103486631+rajeshwariramya@users.noreply.github.com>
2024-10-19 09:30:03 +0530

    Update Fibonacci.java

--- Java_Programs_for_beginners/Fibonacci.java
+++ Java_Programs_for_beginners/Fibonacci.java
@@ -1,15 +1,17 @@
-class FibonacciExample{  
-public static void main(String args[])  
-{    
- int n1=0,n2=1,n3,i,count=10;    
- System.out.print(n1+" "+n2);//printing 0 and 1    
-    
- for(i=2;i<count;++i)//loop starts from 2 because 0 and 1 are already printed    
- {    
-  n3=n1+n2;    
-  System.out.print(" "+n3);    
-  n1=n2;    
-  n2=n3;    
- }    
-  
-}}  
+class FibonacciExample {
+    public static void main(String[] args) {
+        int n1 = 0, n2 = 1, n3, count = 10;
+
+        System.out.print(n1 + " " + n2); // Printing 0 and 1
+
+        for (int i = 2; i < count; ++i) { // Loop starts from 2
+            n3 = n1 + n2; // Next Fibonacci number
+            System.out.print(" " + n3); // Print the next number
+            n1 = n2; // Update n1
+            n2 = n3; // Update n2
+        }
+
+        System.out.println(); // Add a new line at the end
+    }
+}
+
